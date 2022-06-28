4e58b6bfadcfe44a5c9d27428f9a75c1c80ffc96
Nikolay Dimitrov <36077516+NiksonHD@users.noreply.github.com>
2022-06-28 20:00:05 +0300

    Update configPath.py

--- configPath.py
+++ configPath.py
@@ -1,38 +1,38 @@
 #wh
-# mailsPath = "C:\\Users\\a\\PRINT-WEB-SHOP\\"
+mailsPath = "C:\\Users\\a\\PRINT-WEB-SHOP\\"
 
 #dev
-mailsPath = "C:\\Users\\nikso\\python-scripts\\PRINT-WEB-SHOP\\"
+# mailsPath = "C:\\Users\\nikso\\python-scripts\\PRINT-WEB-SHOP\\"
 
 
 
 #wh
-# htmlPath = "C:\\Users\\a\\Music\\NHD\\python-scripts\\web-order.html"
+htmlPath = "C:\\Users\\a\\Music\\NHD\\python-scripts\\web-order.html"
 
 #dev
-htmlPath = "C:\\Users\\nikso\\python-scripts\\web-order.html"
+# htmlPath = "C:\\Users\\nikso\\python-scripts\\web-order.html"
 
 
 
 #wh
-# qrPath = "C:\\Users\\a\\Music\\NHD\\python-scripts\\web-QR-code.png"
+qrPath = "C:\\Users\\a\\Music\\NHD\\python-scripts\\web-QR-code.png"
 
 #dev
-qrPath = "C:\\Users\\nikso\\python-scripts\\web-QR-code.png"
+# qrPath = "C:\\Users\\nikso\\python-scripts\\web-QR-code.png"
 
 
 
 #wh
-# pdfFolder = "C:\\Users\\a\\Music\\NHD\\python-scripts\\pdf-files\\"
+pdfFolder = "C:\\Users\\a\\Music\\NHD\\python-scripts\\pdf-files\\"
 
 #dev
-pdfFolder = "C:\\Users\\nikso\\python-scripts\\pdf-files\\"
+# pdfFolder = "C:\\Users\\nikso\\python-scripts\\pdf-files\\"
 
 #wh
 barcodePath = "C:\\Users\\a\\Music\\NHD\\python-scripts\\barcodes\\"
 
 #dev
-barcodePath = "C:\\Users\\nikso\\python-scripts\\barcodes\\"
+# barcodePath = "C:\\Users\\nikso\\python-scripts\\barcodes\\"
 
 
 # pdfkit.from_file(htmlPath, pdfFolder + plainFilename + ".pdf", configuration=config, options = options )
