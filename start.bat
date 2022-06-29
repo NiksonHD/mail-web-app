46675f418b975508962870c60dab946a8da7c371
Nikolay Dimitrov <36077516+NiksonHD@users.noreply.github.com>
2022-06-29 18:16:31 +0300

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,4 @@
+wkhtmltoimage_set_global_setting(settings, "loadPage.blockLocalFileAccess", "false");
+
+C:\Users\a\AppData\Local\Programs\Python\Python310\python.exe "C:\Users\a\Music\NHD\python-scripts\make-order-last.py"
+REM pause
